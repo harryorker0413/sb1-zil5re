ef89215e2706e11194cdbcbd0a39f0def76d2225
harryorker0413 <166029492+harryorker0413@users.noreply.github.com>
2024-10-31 22:40:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/date', (req, res) => {
+app.get('/dateSSSS', (req, res) => {
   const today = new Date();
   res.json({
     date: today.toISOString().split('T')[0]
